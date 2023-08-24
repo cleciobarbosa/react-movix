c83a6883d37dc9f715fda61863939bc91fef6c6e
cleciobarbosa <clecio.sb@gmail.com>
2023-08-23 22:49:36 -0300

    adicionado carrosell

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ function App() {
             <Route path="/explore/:mediaType" element={<Explore />} />
             <Route path="*" element={<PageNotFound />} />
         </Routes>
-        {/* <Footer /> */}
+        <Footer />
     </BrowserRouter>
 );
 }
