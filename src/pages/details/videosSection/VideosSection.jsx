03fdbdd5dfaf811de245b95a52b3292a048d1be7
cleciobarbosa <clecio.sb@gmail.com>
2023-08-29 21:28:24 -0300

    fix videoPopup folder

--- src/pages/details/videosSection/VideosSection.jsx
+++ src/pages/details/videosSection/VideosSection.jsx
@@ -3,8 +3,8 @@ import React, { useState } from "react";
 import "./style.scss";
 
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
-import Img from "../../../components/lazyLoadImage/Img";
 import VideoPopup from "../../../components/videoPopup/VideoPopup";
+import Img from "../../../components/lazyLoadImage/Img";
 import { PlayIcon } from "../Playbtn";
 
 const VideosSection = ({ data, loading }) => {
