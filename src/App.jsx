2ca33bd384782c6ea71460a209bb6d94842adec9
cleciobarbosa <clecio.sb@gmail.com>
2023-08-29 22:20:51 -0300

    trefix

--- src/App.jsx
+++ src/App.jsx
@@ -13,59 +13,61 @@ import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
 import PageNotFound from "./pages/404/PageNotFound";
 
-
 function App() {
-  const dispatch = useDispatch();
-  const { url } = useSelector((state) => state.home);
+    const dispatch = useDispatch();
+    const { url } = useSelector((state) => state.home);
+    console.log(url);
 
-  useEffect(() => {
-    fetchApiConfig();
-    genresCall();
-  }, []);
+    useEffect(() => {
+        fetchApiConfig();
+        genresCall();
+    }, []);
 
-  const fetchApiConfig = () => {
-    fetchDataFromApi("/configuration").then((res) => {
+    const fetchApiConfig = () => {
+        fetchDataFromApi("/configuration").then((res) => {
+            console.log(res);
 
-      const url = {
-        backdrop: res.images.secure_base_url + "original",
-        poster: res.images.secure_base_url + "original",
-        profile: res.images.secure_base_url + "original",
-      };
+            const url = {
+                backdrop: res.images.secure_base_url + "original",
+                poster: res.images.secure_base_url + "original",
+                profile: res.images.secure_base_url + "original",
+            };
 
-      dispatch(getApiConfiguration(url));
-    });
-  };
-  const genresCall = async () => {
-    let promises = [];
-    let endPoints = ["tv", "movie"];
-    let allGenres = {};
+            dispatch(getApiConfiguration(url));
+        });
+    };
 
-    endPoints.forEach((url) => {
-      promises.push(fetchDataFromApi(`/genre/${url}/list`));
-    });
+    const genresCall = async () => {
+        let promises = [];
+        let endPoints = ["tv", "movie"];
+        let allGenres = {};
 
-    const data = await Promise.all(promises);
-    console.log(data);
-    data.map(({ genres }) => {
-      return genres.map((item) => (allGenres[item.id] = item));
-    });
+        endPoints.forEach((url) => {
+            promises.push(fetchDataFromApi(`/genre/${url}/list`));
+        });
 
-    dispatch(getGenres(allGenres));
-  };
+        const data = await Promise.all(promises);
+        console.log(data);
+        data.map(({ genres }) => {
+            return genres.map((item) => (allGenres[item.id] = item));
+        });
 
-  return (
-    <BrowserRouter>
-        <Header />
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/:mediaType/:id" element={<Details />} />
-            <Route path="/search/:query" element={<SearchResult />} />
-            <Route path="/explore/:mediaType" element={<Explore />} />
-            <Route path="*" element={<PageNotFound />} />
-        </Routes>
-        <Footer />
-    </BrowserRouter>
-);
+        dispatch(getGenres(allGenres));
+    };
+
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/:mediaType/:id" element={<Details />} />
+                <Route path="/search/:query" element={<SearchResult />} />
+                <Route path="/explore/:mediaType" element={<Explore />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
